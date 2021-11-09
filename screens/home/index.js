6f8ab06d3cdd8f60db98e3e0e056283ff102b0b9
Erickson Vital <ericksonrodrigo321@gmail.com>
2021-11-09 05:43:51 -0300

    everything is OK

--- screens/home/index.js
+++ screens/home/index.js
@@ -1,37 +1,39 @@
-import React, {useState, useEffect} from 'react'
-import { View, Text, StyleSheet, FlatList} from 'react-native'
-import api from '../../src/services/api';
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+import { api } from '../../src/services/api';
 
 const index = () => {
-
     const [users, setUsers] = useState([]);
 
-    useEffect(() =>{
-        (async () =>{
+    useEffect(() => {
+        (async () => {
             const { data } = await api.get('/user')
-            setUsers(data)
+            setUsers(data);
         })()
     }, [])
+
 
     return (
         <View style={styles.container}>
             <View style={styles.flat}>
-                <FlatList 
+                <FlatList
                     data={users}
-                    renderItem={({item}) => <Text>{item.email}</Text>}
+                    renderItem={({ item }) => <Text style={{fontSize: 19}}>{item.email}</Text>}
                 />
             </View>
         </View>
     )
 }
 
-export default index
+export default index;
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
-        justifyContent: 'center'
+        backgroundColor: '#fff',
+        justifyContent: 'center',
     },
-    flat:{
-        alignItems: 'center'
-    },
-})
+    flat: {
+        alignItems: 'center',
+    }
+});
